924ff0aa8091927987d36a46a5760647bf5049fa
AposVinc <apos.vincenzo@gmail.com>
2020-06-28 19:51:22 +0200

    Create Service.java

---
+++ src/main/java/it/univaq/disim/GymREST/business/Service.java
@@ -0,0 +1,35 @@
+package it.univaq.disim.GymREST.business;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Service {
+
+    private Connection connection;
+
+    public Service(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:8889/gymportal", "gymportal", "gymportal");
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnetion() throws SQLException {
+        connection.close();
+        if (connection.isClosed()){
+            System.out.println("Connection Closed");
+        }
+    }
+}
