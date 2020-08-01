8e854e19dbb2808cf47549d9cc51056ad05be4c2
AposVinc <apos.vincenzo@gmail.com>
2020-08-01 20:49:08 +0200

    aggiunta metodo per stampare ResultSet

--- src/main/java/it/univaq/disim/GymREST/business/Service.java
+++ src/main/java/it/univaq/disim/GymREST/business/Service.java
@@ -1,5 +1,9 @@
 package it.univaq.disim.GymREST.business;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
 public class Service {
 
     protected static void loadDriver() {
@@ -8,6 +12,20 @@ public class Service {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             System.exit(1);
+        }
+    }
+
+    //metodo per lo sviluppo, da eliminare
+    public void printRS(ResultSet rs) throws SQLException {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        while (rs.next()) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1) System.out.print(",  ");
+                String columnValue = rs.getString(i);
+                System.out.print(columnValue + " " + rsmd.getColumnName(i));
+            }
+            System.out.println("");
         }
     }
 
