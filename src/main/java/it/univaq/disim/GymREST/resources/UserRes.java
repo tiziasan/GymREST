51beb9e51d1d946c00ba3285d56a32e53caf244c
AposVinc <apos.vincenzo@gmail.com>
2020-08-26 18:50:24 +0200

    Revert "Update UserRes.java"
    This reverts commit 3c28755f542adeb344967ce5356abc5e96e4f2ee.

--- src/main/java/it/univaq/disim/GymREST/resources/UserRes.java
+++ src/main/java/it/univaq/disim/GymREST/resources/UserRes.java
@@ -7,15 +7,15 @@ import it.univaq.disim.GymREST.business.impl.FeedbackCourseServiceImpl;
 import it.univaq.disim.GymREST.business.impl.FeedbackGymServiceImpl;
 import it.univaq.disim.GymREST.business.impl.UserServiceImpl;
 import it.univaq.disim.GymREST.model.User;
-import it.univaq.disim.GymREST.security.Auth;
 
+import javax.annotation.security.PermitAll;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
 
-@Auth
 @Path("users")
+@PermitAll
 public class UserRes {
 
     private static final String urlDB = "jdbc:mysql://127.0.0.1:8889/gymportal?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
